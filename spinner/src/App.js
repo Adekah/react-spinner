bcd8fa07b2b187efbaaa708c5b48fc060cb74c79
Hüseyin Adeka <adekahuseyin@hotmail.com>
2022-06-08 00:04:08 +0300

    Simple Update
    Simple Update

--- spinner/src/App.js
+++ spinner/src/App.js
@@ -3,86 +3,62 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
-state={
-  name:"circle"
-}
-startRotate=()=>{
-  this.setState({
-    name:"circle start-rotate"
-  });
-
-  setTimeout(()=>{
+  state = {
+    name: "circle",
+    values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  }
+  startRotate = () => {
     this.setState({
-      name:"circle start-rotate stop-rotate"
-    })
-  },Math.floor(Math.random()*5000)+1);
-}
+      name: "circle start-rotate"
+    });
+
+    setTimeout(() => {
+      this.setState({
+        name: "circle start-rotate stop-rotate"
+      })
+    }, Math.floor(Math.random() * 5000) + 1);
+  }
   render() {
     return (
       <div>
         <div className='arrow'></div>
         <ul className={this.state.name}>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">1</div>
+            <div className='text'>{this.state.values[0]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">2</div>
+            <div className='text'>{this.state.values[1]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">3</div>
+            <div className='text'>{this.state.values[2]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">4</div>
+            <div className='text'>{this.state.values[3]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">5</div>
+            <div className='text'>{this.state.values[4]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">6</div>
+            <div className='text'>{this.state.values[5]} Puan </div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">7</div>
+            <div className='text'>{this.state.values[6]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">8</div>
+            <div className='text'>{this.state.values[7]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">9</div>
+            <div className='text'>{this.state.values[8]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">10</div>
+            <div className='text'>{this.state.values[9]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">11</div>
+            <div className='text'>{this.state.values[10]} Puan</div>
           </li>
           <li>
-            <div className='text'
-              contentEditable="true"
-              spellCheck="false">12</div>
+            <div className='text'>{this.state.values[11]} Puan</div>
           </li>
-
         </ul>
         <button className='spin-button' onClick={this.startRotate}>SPIN</button>
       </div>
